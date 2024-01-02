eb23542c2e397e60191ec71425852d48d292b7ba
Aakash <aakashanand1995@gmail.com>
2024-01-02 14:04:40 +0530

    updated the home page

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 app.set("views", rootDirectory + "/views");
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.use(
